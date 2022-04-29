4f7d5d80a314b0e2e505b33230ee37f560257fae
DmitriVanGuard <dimavanguard@gmail.com>
2022-04-29 18:34:50 +0300

    webkit updated

--- src/Sidebar/Layouts/utils.ts
+++ src/Sidebar/Layouts/utils.ts
@@ -1,6 +1,6 @@
 import { saveValue, getSavedValue } from 'webkit/utils/localStorage'
 import { queryShortLayout } from '@/api/layouts'
-import { getSavedRecentLayoutIds } from '@/Layouts/utils'
+import { getSavedRecentLayoutIds, removeRecentLayoutId } from '@/Layouts/utils'
 
 export enum Tab {
   MyLibrary = 'My Library',
@@ -25,7 +25,12 @@ export const newExploreGraph = () => ({
 const emptyLayoutsFilter = (layouts) => layouts.filter(Boolean)
 export function queryRecentLayouts(): Promise<SAN.Layout[]> {
   const ids = getSavedRecentLayoutIds()
-  const query = (id: number) => queryShortLayout(id).catch(() => null)
+  const query = (id: number) =>
+    queryShortLayout(id).catch(() => {
+      removeRecentLayoutId(id)
+      return null
+    })
+
   return ids.length ? Promise.all(ids.map(query)).then(emptyLayoutsFilter) : Promise.resolve([])
 }
 
