0be75f5b602355e623b577857b1a9d1a3c917cc6
Igor <ihqrk@pm.me>
2022-09-12 14:56:33 +0300

    Updated blockchain name

--- src/MasterSelectorDialog/Blockchains.svelte
+++ src/MasterSelectorDialog/Blockchains.svelte
@@ -2,7 +2,7 @@
   import Svg from 'webkit/ui/Svg/svelte'
   import Tooltip from 'webkit/ui/Tooltip/svelte'
   import ProjectIcon from 'webkit/ui/ProjectIcon.svelte'
-  import { queryAvailableBlockchains } from '@/api/blockchains'
+  import { BLOCKCHAINS_NAMES, queryAvailableBlockchains } from '@/api/blockchains'
 
   export let blockchain = undefined
   export let inputNode: HTMLInputElement
@@ -32,7 +32,7 @@
       {#if blockchain}
         <div class="row v-center">
           <ProjectIcon slug={blockchain.slug} size={16} class="mrg-s mrg--r" />
-          {blockchain.blockchain}
+          {BLOCKCHAINS_NAMES[blockchain.infrastructure.toLowerCase()]}
         </div>
       {:else}
         All blockchains
@@ -46,7 +46,7 @@
       {#each blockchains as item (item.slug)}
         <div class="btn-ghost row v-center" on:click={() => onBlockchainSelect(item)}>
           <ProjectIcon slug={item.slug} size={24} class="mrg-s mrg--r" />
-          {item.blockchain}
+          {BLOCKCHAINS_NAMES[item.infrastructure.toLowerCase()]}
         </div>
       {/each}
     </svelte:fragment>
