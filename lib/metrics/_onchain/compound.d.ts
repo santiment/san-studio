ffa8143fb37ce10a544945245d8ae3109020d4d5
DmitriVanGuard <dimavanguard@gmail.com>
2022-11-11 20:05:21 +0300

    Library release

---
+++ lib/metrics/_onchain/compound.d.ts
@@ -0,0 +1,18 @@
+export declare const CompoundMetric: {
+    compound_action_deposits: import("../utils").Node<Studio.Metric, "compound_action_deposits">;
+    compound_action_liquidations: import("../utils").Node<Studio.Metric, "compound_action_liquidations">;
+    compound_action_new_debt: import("../utils").Node<Studio.Metric, "compound_action_new_debt">;
+    compound_action_repayments: import("../utils").Node<Studio.Metric, "compound_action_repayments">;
+    compound_total_supplied: import("../utils").Node<Studio.Metric, "compound_total_supplied">;
+    compound_total_borrowed: import("../utils").Node<Studio.Metric, "compound_total_borrowed">;
+    compound_action_deposits_usd: import("../utils").Node<Studio.Metric, "compound_action_deposits_usd">;
+    compound_action_liquidations_usd: import("../utils").Node<Studio.Metric, "compound_action_liquidations_usd">;
+    compound_action_new_debt_usd: import("../utils").Node<Studio.Metric, "compound_action_new_debt_usd">;
+    compound_action_repayments_usd: import("../utils").Node<Studio.Metric, "compound_action_repayments_usd">;
+    compound_total_deposits_usd: import("../utils").Node<Studio.Metric, "compound_total_deposits_usd">;
+    compound_total_liquidations_usd: import("../utils").Node<Studio.Metric, "compound_total_liquidations_usd">;
+    compound_total_new_debt_usd: import("../utils").Node<Studio.Metric, "compound_total_new_debt_usd">;
+    compound_total_repayments_usd: import("../utils").Node<Studio.Metric, "compound_total_repayments_usd">;
+    compound_total_supplied_usd: import("../utils").Node<Studio.Metric, "compound_total_supplied_usd">;
+    compound_total_borrowed_usd: import("../utils").Node<Studio.Metric, "compound_total_borrowed_usd">;
+};
