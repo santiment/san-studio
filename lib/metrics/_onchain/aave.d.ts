fc534ed73c7fcd1105b5076c88f3dfa145a1f9b7
DmitriVanGuard <dimavanguard@gmail.com>
2022-11-11 11:57:09 +0300

    Library release

--- lib/metrics/_onchain/aave.d.ts
+++ lib/metrics/_onchain/aave.d.ts
@@ -1,4 +1,5 @@
 export declare const AaveMetric: {
+    aave_v2_action_deposits: import("../utils").Node<Studio.Metric, "aave_v2_action_deposits">;
     aave_v2_action_liquidations: import("../utils").Node<Studio.Metric, "aave_v2_action_liquidations">;
     aave_v2_action_new_debt: import("../utils").Node<Studio.Metric, "aave_v2_action_new_debt">;
     aave_v2_action_repayments: import("../utils").Node<Studio.Metric, "aave_v2_action_repayments">;
