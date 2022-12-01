e2501a5e874f43428dfa08a5e3d3f539e38c2e51
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-01 23:49:20 +0300

    Exporting drawLegend

--- src/Chart/index.svelte
+++ src/Chart/index.svelte
@@ -33,7 +33,7 @@
   if (SSR_CHART) {
     width = chart.canvasWidth
     height = chart.canvasHeight
-    padding = DEFAULT_PADDING
+    padding = Object.assign({}, DEFAULT_PADDING, chart.padding)
     setupChart(chart)
     chart.setPadding = (newPadding) => {
       padding = newPadding
