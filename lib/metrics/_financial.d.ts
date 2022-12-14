5aa45f4328c7566b20cdb62ff2dbeedbb4cc520c
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-14 12:06:17 +0300

    Library release

--- lib/metrics/_financial.d.ts
+++ lib/metrics/_financial.d.ts
@@ -5,6 +5,10 @@ export declare const FinancialMetric: {
     nft_collection_min_price_usd: import("./utils").Node<Studio.Metric, "nft_collection_min_price_usd">;
     nft_collection_max_price_usd: import("./utils").Node<Studio.Metric, "nft_collection_max_price_usd">;
     nft_collection_avg_price_usd: import("./utils").Node<Studio.Metric, "nft_collection_avg_price_usd">;
+    price_volatility_1d: import("./utils").Node<Studio.Metric, "price_volatility_1d">;
+    price_volatility_1w: import("./utils").Node<Studio.Metric, "price_volatility_1w">;
+    price_volatility_2w: import("./utils").Node<Studio.Metric, "price_volatility_2w">;
+    price_volatility_4w: import("./utils").Node<Studio.Metric, "price_volatility_4w">;
     price_usd: import("./utils").Node<Studio.Metric, "price_usd">;
     price_btc: import("./utils").Node<Studio.Metric, "price_btc">;
     price_eth: import("./utils").Node<Studio.Metric, "price_eth">;
