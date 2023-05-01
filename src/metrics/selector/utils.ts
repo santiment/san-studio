6df42a0c18fd504fce2dcc2425aa5567db893650
DmitriVanGuard <dimavanguard@gmail.com>
2023-05-01 13:02:11 +0300

    Checking for MouseEvent first to display tooltip

--- src/metrics/selector/utils.ts
+++ src/metrics/selector/utils.ts
@@ -20,7 +20,6 @@ function addItemToSelectorGraph(
   if (subitems) categoryItems.push(...subitems.filter(filter))
 }
 
-const indexSorter = (a: string, b: string) => (MetricIndex[a] || -1) - (MetricIndex[b] || -1)
 export function getMetricsSelectorGraph(
   metricKeys: string[],
   options: { [key: string]: string },
@@ -35,7 +34,7 @@ export function getMetricsSelectorGraph(
   }
 
   const filter = ({ checkIsVisible }: any) => (checkIsVisible ? checkIsVisible(options) : true)
-  const { length } = metricKeys.sort(indexSorter)
+  const { length } = metricKeys
   for (let i = 0; i < length; i++) {
     const metricKey = metricKeys[i]
     const node = (ReplacementNode[metricKey] || SelectorNode[metricKey]) as
