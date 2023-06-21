5f80ac7d0944f323bc1f418636d0e6b248c91aa7
DmitriVanGuard <dimavanguard@gmail.com>
2023-06-21 12:04:02 +0200

    Library release

--- lib/sharing/metrics.d.ts
+++ lib/sharing/metrics.d.ts
@@ -1,3 +1,4 @@
+import { MetricType } from './../metrics/utils';
 export declare function shareMetrics(metrics: Studio.Metric[]): string[];
 export declare function parseMetric(key: string | TupleData[]): any | undefined;
 declare type TupleData = string | string[];
@@ -5,4 +6,5 @@ export declare function parseMetricKey(key: string): TupleData[];
 export declare function getTupleData(args: string[], cursor?: {
     i: number;
 }): TupleData[];
+export declare function getMetricKeyData(key: string): TupleData[] | (string | MetricType)[];
 export {};
