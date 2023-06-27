10a9991e88588f97ec2c50086aac0458c32ae446
DmitriVanGuard <dimavanguard@gmail.com>
2023-06-27 09:52:01 +0200

    Using input calendar

--- src/history/ctx.ts
+++ src/history/ctx.ts
@@ -1,6 +1,8 @@
 import type { newHistory } from 'webkit/ui/history'
 
+import { getContext } from 'svelte'
 import { CONTEXT } from 'webkit/ui/history'
 import { getStudioContext } from '@/context'
 
-export const getHistoryContext = () => getStudioContext(CONTEXT) as ReturnType<typeof newHistory>
+export const getHistoryContext = () =>
+  (getStudioContext(CONTEXT) as ReturnType<typeof newHistory>) || getContext(CONTEXT)
