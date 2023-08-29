4ea659f2fcfbfe9daf1d9e93e92a47025c30d15c
DmitriVanGuard <dimavanguard@gmail.com>
2023-08-29 10:24:01 +0300

    Library release

--- lib/metrics/_onchain/compound.d.ts
+++ lib/metrics/_onchain/compound.d.ts
@@ -21,3 +21,24 @@ export declare const CompoundMetric: {
     compound_borrow_apy: import("../utils").Node<Studio.Metric, "compound_borrow_apy">;
     compound_active_addresses: import("../utils").Node<Studio.Metric, "compound_active_addresses">;
 };
+export declare const Compound3Metric: {
+    compound_v3_action_deposits: import("../utils").Node<Studio.Metric, "compound_v3_action_deposits">;
+    compound_v3_action_liquidations: import("../utils").Node<Studio.Metric, "compound_v3_action_liquidations">;
+    compound_v3_action_new_debt: import("../utils").Node<Studio.Metric, "compound_v3_action_new_debt">;
+    compound_v3_action_repayments: import("../utils").Node<Studio.Metric, "compound_v3_action_repayments">;
+    compound_v3_action_deposits_usd: import("../utils").Node<Studio.Metric, "compound_v3_action_deposits_usd">;
+    compound_v3_action_liquidations_usd: import("../utils").Node<Studio.Metric, "compound_v3_action_liquidations_usd">;
+    compound_v3_action_new_debt_usd: import("../utils").Node<Studio.Metric, "compound_v3_action_new_debt_usd">;
+    compound_v3_action_repayments_usd: import("../utils").Node<Studio.Metric, "compound_v3_action_repayments_usd">;
+    compound_v3_total_deposits_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_deposits_usd">;
+    compound_v3_total_liquidations_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_liquidations_usd">;
+    compound_v3_total_new_debt_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_new_debt_usd">;
+    compound_v3_total_repayments_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_repayments_usd">;
+    compound_v3_total_supplied: import("../utils").Node<Studio.Metric, "compound_v3_total_supplied">;
+    compound_v3_total_supplied_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_supplied_usd">;
+    compound_v3_total_borrowed: import("../utils").Node<Studio.Metric, "compound_v3_total_borrowed">;
+    compound_v3_total_borrowed_usd: import("../utils").Node<Studio.Metric, "compound_v3_total_borrowed_usd">;
+    compound_v3_protocol_total_supplied_usd: import("../utils").Node<Studio.Metric, "compound_v3_protocol_total_supplied_usd">;
+    compound_v3_protocol_total_borrowed_usd: import("../utils").Node<Studio.Metric, "compound_v3_protocol_total_borrowed_usd">;
+    compound_v3_active_addresses: import("../utils").Node<Studio.Metric, "compound_v3_active_addresses">;
+};
