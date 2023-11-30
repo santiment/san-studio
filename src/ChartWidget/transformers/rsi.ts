b9abc3d17dfcfff82468c42e50a0c22821cecbe0
DmitriVanGuard <dimavanguard@gmail.com>
2023-11-30 22:35:12 +0100

    feat(metrics): Preserving user set rsi settings value

--- src/ChartWidget/transformers/rsi.ts
+++ src/ChartWidget/transformers/rsi.ts
@@ -2,7 +2,8 @@ export function transformRsiSettings(metric: Studio.Metric, metricSettings: Stud
   const baseKey = (metric.base || metric).key
   if (baseKey.includes('rsi_') === false) return
 
-  if (metricSettings.interval) return
+  if (metricSettings.interval || metricSettings.__appliedInterval) return
 
   metricSettings.interval = (metric.base || metric).defaultInterval
+  metricSettings.__appliedInterval = true
 }
