924261f55da45a7e25ebd6886e669292fa57143e
Vlad Keliy <vekkele@gmail.com>
2024-04-02 14:06:39 +0300

    Library release

--- lib/metrics/_onchain/makerDao.d.ts
+++ lib/metrics/_onchain/makerDao.d.ts
@@ -1,20 +1,11 @@
 export declare const MakerDaoMetric: {
-    mcd_locked_token: import("../utils").Node<Studio.Metric, "mcd_locked_token">;
-    scd_locked_token: import("../utils").Node<Studio.Metric, "scd_locked_token">;
-    mcd_collat_ratio: import("../utils").Node<Studio.Metric, "mcd_collat_ratio">;
-    mcd_collat_ratio_weth: import("../utils").Node<Studio.Metric, "mcd_collat_ratio_weth">;
-    mcd_collat_ratio_sai: import("../utils").Node<Studio.Metric, "mcd_collat_ratio_sai">;
-    scd_collat_ratio: import("../utils").Node<Studio.Metric, "scd_collat_ratio">;
-    mcd_dsr: import("../utils").Node<Studio.Metric, "mcd_dsr">;
-    mcd_stability_fee: import("../utils").Node<Studio.Metric, "mcd_stability_fee">;
-    dai_created: import("../utils").Node<Studio.Metric, "dai_created">;
-    dai_repaid: import("../utils").Node<Studio.Metric, "dai_repaid">;
-    mcd_liquidation: import("../utils").Node<Studio.Metric, "mcd_liquidation">;
-    total_dai_created: import("../utils").Node<Studio.Metric, "total_dai_created">;
     makerdao_action_deposits: import("../utils").Node<Studio.Metric, "makerdao_action_deposits">;
     makerdao_action_liquidations: import("../utils").Node<Studio.Metric, "makerdao_action_liquidations">;
     makerdao_action_new_debt: import("../utils").Node<Studio.Metric, "makerdao_action_new_debt">;
+    dai_created: import("../utils").Node<Studio.Metric, "dai_created">;
     makerdao_action_repayments: import("../utils").Node<Studio.Metric, "makerdao_action_repayments">;
+    dai_repaid: import("../utils").Node<Studio.Metric, "dai_repaid">;
+    total_dai_created: import("../utils").Node<Studio.Metric, "total_dai_created">;
     makerdao_action_deposits_usd: import("../utils").Node<Studio.Metric, "makerdao_action_deposits_usd">;
     makerdao_action_liquidations_usd: import("../utils").Node<Studio.Metric, "makerdao_action_liquidations_usd">;
     makerdao_action_new_debt_usd: import("../utils").Node<Studio.Metric, "makerdao_action_new_debt_usd">;
@@ -29,6 +20,9 @@ export declare const MakerDaoMetric: {
     makerdao_total_borrowed_usd: import("../utils").Node<Studio.Metric, "makerdao_total_borrowed_usd">;
     makerdao_protocol_total_supplied_usd: import("../utils").Node<Studio.Metric, "makerdao_protocol_total_supplied_usd">;
     makerdao_protocol_total_borrowed_usd: import("../utils").Node<Studio.Metric, "makerdao_protocol_total_borrowed_usd">;
+    mcd_collat_ratio: import("../utils").Node<Studio.Metric, "mcd_collat_ratio">;
+    mcd_stability_fee: import("../utils").Node<Studio.Metric, "mcd_stability_fee">;
+    mcd_dsr: import("../utils").Node<Studio.Metric, "mcd_dsr">;
     makerdao_active_addresses: import("../utils").Node<Studio.Metric, "makerdao_active_addresses">;
 };
 export declare const MakerDaoDsrMetric: {
