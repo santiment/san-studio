e642c50667e7bb6417f87d82aa5bd735ded5307e
filips121 <filip.s@santiment.net>
2024-06-19 17:16:27 +0200

    Change default style to bar chart

--- src/metrics/_onchain/compound.ts
+++ src/metrics/_onchain/compound.ts
@@ -5,31 +5,39 @@ export const CompoundMetric = each(
   {
     compound_action_deposits: {
       label: 'Compound Deposits',
+      node: 'bar',
     },
     compound_action_deposits_usd: {
       label: 'Compound Deposits in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_action_liquidations: {
       label: 'Compound Liquidations',
+      node: 'bar',
     },
     compound_action_liquidations_usd: {
       label: 'Compound Liquidations in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_action_new_debt: {
       label: 'Compound New Debt',
+      node: 'bar',
     },
     compound_action_new_debt_usd: {
       label: 'Compound New Debt in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_action_repayments: {
       label: 'Compound Repayments',
+      node: 'bar',
     },
     compound_action_repayments_usd: {
       label: 'Compound Repayments in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_total_supplied: {
       label: 'Compound Total Supplied',
@@ -64,18 +72,22 @@ export const CompoundMetric = each(
     compound_total_deposits_usd: {
       label: 'Compound Protocol Total Deposits in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_total_liquidations_usd: {
       label: 'Compound Protocol Total Liquidations in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_total_new_debt_usd: {
       label: 'Compound Protocol Total New Debt in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_total_repayments_usd: {
       label: 'Compound Protocol Total Repayments in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_protocol_total_supplied_usd: {
       label: 'Compound Protocol Total Supplied in USD',
@@ -87,6 +99,7 @@ export const CompoundMetric = each(
     },
     compound_active_addresses: {
       label: 'Compound Protocol Active Addresses',
+      node: 'bar',
     },
     compound_total_protocol_revenue_usd: {
       label: 'Compound Protocol Total Revenue in USD',
@@ -107,31 +120,39 @@ export const Compound3Metric = each(
   {
     compound_v3_action_deposits: {
       label: 'Compound v3 Deposits',
+      node: 'bar',
     },
     compound_v3_action_deposits_usd: {
       label: 'Compound v3 Deposits in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_action_liquidations: {
       label: 'Compound v3 Liquidations',
+      node: 'bar',
     },
     compound_v3_action_liquidations_usd: {
       label: 'Compound v3 Liquidations in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_action_new_debt: {
       label: 'Compound v3 New Debt',
+      node: 'bar',
     },
     compound_v3_action_new_debt_usd: {
       label: 'Compound v3 New Debt in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_action_repayments: {
       label: 'Compound v3 Repayments',
+      node: 'bar',
     },
     compound_v3_action_repayments_usd: {
       label: 'Compound v3 Repayments in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_total_supplied: {
       label: 'Compound v3 Total Supplied',
@@ -151,18 +172,22 @@ export const Compound3Metric = each(
     compound_v3_total_deposits_usd: {
       label: 'Compound v3 Protocol Total Deposits in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_total_liquidations_usd: {
       label: 'Compound v3 Protocol Total Liquidations in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_total_new_debt_usd: {
       label: 'Compound v3 Protocol Total New Debt in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_total_repayments_usd: {
       label: 'Compound v3 Protocol Total Repayments in USD',
       formatter: usdFormatter,
+      node: 'bar',
     },
     compound_v3_protocol_total_supplied_usd: {
       label: 'Compound v3 Protocol Total Supplied in USD',
@@ -186,6 +211,7 @@ export const Compound3Metric = each(
     },
     compound_v3_active_addresses: {
       label: 'Compound v3 Protocol Active Addresses',
+      node: 'bar',
     },
   },
   (metric: Studio.Metric) => {
