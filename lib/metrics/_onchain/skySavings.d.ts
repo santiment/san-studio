7a45d3e879473ad81644d20e2618535b54feb792
DmitriVanGuard <dimavanguard@gmail.com>
2024-10-11 13:33:58 +0200

    Library release

--- lib/metrics/_onchain/skySavings.d.ts
+++ lib/metrics/_onchain/skySavings.d.ts
@@ -2,4 +2,5 @@ export declare const SkySavingsMetric: {
     sky_savings_deposits: import("../utils").Node<Studio.Metric, "sky_savings_deposits">;
     sky_savings_withdrawals: import("../utils").Node<Studio.Metric, "sky_savings_withdrawals">;
     sky_savings_total_supplied: import("../utils").Node<Studio.Metric, "sky_savings_total_supplied">;
+    sky_savings_apy: import("../utils").Node<Studio.Metric, "sky_savings_apy">;
 };
