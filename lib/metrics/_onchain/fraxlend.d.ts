64f5a058475f6a81f7fc7fc08f1d0de3ffd7ceb6
DmitriVanGuard <dimavanguard@gmail.com>
2024-11-20 20:29:42 +0100

    Library release

--- lib/metrics/_onchain/fraxlend.d.ts
+++ lib/metrics/_onchain/fraxlend.d.ts
@@ -7,10 +7,15 @@ export declare const FraxlendMetric: {
     fraxlend_action_new_debt_usd: import("../utils").Node<Studio.Metric, "fraxlend_action_new_debt_usd">;
     fraxlend_action_repayments: import("../utils").Node<Studio.Metric, "fraxlend_action_repayments">;
     fraxlend_action_repayments_usd: import("../utils").Node<Studio.Metric, "fraxlend_action_repayments_usd">;
+    fraxlend_total_supplied: import("../utils").Node<Studio.Metric, "fraxlend_total_supplied">;
+    fraxlend_total_supplied_usd: import("../utils").Node<Studio.Metric, "fraxlend_total_supplied_usd">;
+    fraxlend_total_borrowed_against_collateral: import("../utils").Node<Studio.Metric, "fraxlend_total_borrowed_against_collateral">;
     fraxlend_borrow_apy: import("../utils").Node<Studio.Metric, "fraxlend_borrow_apy">;
     fraxlend_total_deposits_usd: import("../utils").Node<Studio.Metric, "fraxlend_total_deposits_usd">;
     fraxlend_total_liquidations_usd: import("../utils").Node<Studio.Metric, "fraxlend_total_liquidations_usd">;
     fraxlend_total_new_debt_usd: import("../utils").Node<Studio.Metric, "fraxlend_total_new_debt_usd">;
     fraxlend_total_repayments_usd: import("../utils").Node<Studio.Metric, "fraxlend_total_repayments_usd">;
+    fraxlend_protocol_total_supplied_usd: import("../utils").Node<Studio.Metric, "fraxlend_protocol_total_supplied_usd">;
+    fraxlend_protocol_total_borrowed_usd: import("../utils").Node<Studio.Metric, "fraxlend_protocol_total_borrowed_usd">;
     fraxlend_active_addresses: import("../utils").Node<Studio.Metric, "fraxlend_active_addresses">;
 };
