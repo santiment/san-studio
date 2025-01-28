175f75ff0258f877daea2426d07417f5dc20555d
filips121 <filip.s@santiment.net>
2025-01-28 12:26:49 +0100

    Add Pendle markets metrics to the Sanbase

---
+++ src/metrics/_onchain/pendleMarkets.ts
@@ -0,0 +1,27 @@
+import { Node } from '@/Chart/nodes'
+import { each } from '../utils'
+
+export const PendleMarketsMetric = each(
+  {
+    pendle_total_markets_tvl: {
+      label: 'Pendle Total Markets TVL',
+      node: Node.LINE,
+    },
+    pendle_underlying_apy: {
+      label: 'Pendle Underlying APY',
+      node: Node.LINE,
+    },
+    pendle_implied_apy: {
+      label: 'Pendle Implied APY',
+      node: Node.LINE,
+    },
+    pendle_yield_spread: {
+      label: 'Pendle Yield Spread',
+      node: Node.LINE,
+    },
+  },
+  (metric: Studio.Metric) => {
+    metric.group = 'Pendle Markets'
+    metric.node = metric.node || Node.BAR
+  },
+)
