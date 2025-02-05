9d1816f01ca937bb79f0294d406427c32b5d31c0
DmitriVanGuard <dimavanguard@gmail.com>
2025-02-05 13:28:22 +0100

    Library release

---
+++ lib/metrics/_onchain/fluid.d.ts
@@ -0,0 +1,15 @@
+export declare const FluidMetric: {
+    fluid_action_deposits: import("../utils").Node<Studio.Metric, "fluid_action_deposits">;
+    fluid_action_deposits_usd: import("../utils").Node<Studio.Metric, "fluid_action_deposits_usd">;
+    fluid_action_liquidations: import("../utils").Node<Studio.Metric, "fluid_action_liquidations">;
+    fluid_action_liquidations_usd: import("../utils").Node<Studio.Metric, "fluid_action_liquidations_usd">;
+    fluid_action_new_debt: import("../utils").Node<Studio.Metric, "fluid_action_new_debt">;
+    fluid_action_new_debt_usd: import("../utils").Node<Studio.Metric, "fluid_action_new_debt_usd">;
+    fluid_action_repayments: import("../utils").Node<Studio.Metric, "fluid_action_repayments">;
+    fluid_action_repayments_usd: import("../utils").Node<Studio.Metric, "fluid_action_repayments_usd">;
+    fluid_total_deposits_usd: import("../utils").Node<Studio.Metric, "fluid_total_deposits_usd">;
+    fluid_total_liquidations_usd: import("../utils").Node<Studio.Metric, "fluid_total_liquidations_usd">;
+    fluid_total_new_debt_usd: import("../utils").Node<Studio.Metric, "fluid_total_new_debt_usd">;
+    fluid_total_repayments_usd: import("../utils").Node<Studio.Metric, "fluid_total_repayments_usd">;
+    fluid_active_addresses: import("../utils").Node<Studio.Metric, "fluid_active_addresses">;
+};
