1a7d01d006941fe58f9e29bc8d84dfeb6c3f19f0
DmitriVanGuard <dimavanguard@gmail.com>
2025-08-18 15:46:32 +0300

    Library release

---
+++ lib/metrics/_onchain/crvusdSavings.d.ts
@@ -0,0 +1,5 @@
+export declare const CrvUSDSavingsMetric: {
+    crvusd_savings_total_supplied: import("../utils").Node<Studio.Metric, "crvusd_savings_total_supplied">;
+    crvusd_savings_distributions: import("../utils").Node<Studio.Metric, "crvusd_savings_distributions">;
+    crvusd_savings_apy: import("../utils").Node<Studio.Metric, "crvusd_savings_apy">;
+};
